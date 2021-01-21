024f1c44562cdb0f6593022aecd3aee139150359
AmirO8 <AmirO8@users.noreply.github.com>
2021-01-20 18:46:01 -0800

    creating routes

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+#def hello_world():
+    #return 'Hello world'
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
